a2a148459650583e8b6d5395d3b6eaf09958ae93
Gustavo Baseggio <gbaseggio@gmail.com>
2016-07-15 17:03:36 -0300

    Add todo model and POST todo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -53,16 +54,14 @@ app.get('/todos/:id', function(req, res) {
 // POST /todos
 // -----------------------------------------------------------------------------
 app.post('/todos', function(req, res) {
-  var body = _.pick(req.body,'description','completed');
+    var body = _.pick(req.body,'description','completed');
 
-  if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-    return res.status(400).send();
-  }
-  body.description = body.description.trim();
-  body.id = todoNextId++;
-  todos.push(body);
-
-  res.json(body);
+    db.todo.create(body).then(function(todo) {
+        res.json(todo.toJSON());    
+    }, function(e){
+        res.status(400).json(e);
+    });
+    
 });
 
 // -----------------------------------------------------------------------------
@@ -114,8 +113,11 @@ app.put('/todos/:id', function(req, res) {
 });
 
 // -----------------------------------------------------------------------------
-// Start the server
+// Sync DB and Start the server
 // -----------------------------------------------------------------------------
-app.listen(PORT, function(){
-  console.log('Express listening on port: ' + PORT);
+db.sequelize.sync().then( function () {
+    console.log('DB synced.');
+    app.listen(PORT, function(){
+        console.log('Express listening on port: ' + PORT);
+    });
 });
