92612613819154d38d1ce2d1839158c970da230a
Gustavo Baseggio <gbaseggio@gmail.com>
2016-08-18 15:22:36 -0300

    Add user model and POST user

--- server.js
+++ server.js
@@ -130,6 +130,19 @@ app.put('/todos/:id', function(req, res) {
 });
 
 // -----------------------------------------------------------------------------
+// POST /users
+// -----------------------------------------------------------------------------
+app.post('/users', function(req, res) {
+    var body = _.pick(req.body,'email','password');
+
+    db.user.create(body).then(function(user) {
+        res.json(user.toJSON());    
+    }, function(e){
+        res.status(400).json(e);
+    });
+});
+
+// -----------------------------------------------------------------------------
 // Sync DB and Start the server
 // -----------------------------------------------------------------------------
 db.sequelize.sync().then( function () {
