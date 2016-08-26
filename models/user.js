09a404041b3f9839c80551461dca69e7c818ec8c
Gustavo Baseggio <gbaseggio@gmail.com>
2016-08-26 15:01:24 -0300

    added token generation

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (sequelize, DataTypes) {
     var user = sequelize.define('user', {
@@ -66,6 +68,21 @@ module.exports = function (sequelize, DataTypes) {
             toPublicJSON: function() {
                 var json = this.toJSON();
                 return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
+            },
+            generateToken: function(type) {
+                if(!_.isString(type)){
+                    return undefined;
+                }
+                try {
+                    var stringData = JSON.stringify({id: this.get('id'), type: type});
+                    var encryptedData = cryptojs.AES.encrypt(stringData, 'p123456!@#$').toString();
+                    var token = jwt.sign({
+                        token: encryptedData
+                    },'1q2w3e4r5t');
+                    return token;
+                } catch(e) { 
+                    return undefined;
+                }
             }
         }
     });
